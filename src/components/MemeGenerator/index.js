2deb4dc8d879b3e559ffbabe4d106964eafa4a10
sandeep pucha <sandeepsai1509@gmail.com>
2023-03-23 08:09:20 +0000

    solution

---
+++ src/components/MemeGenerator/index.js
@@ -0,0 +1,187 @@
+import {Component} from 'react'
+
+import {
+  AppContainer,
+  MemeGeneratorContainer,
+  Heading,
+  FormAndMemeContainer,
+  MemeContainer,
+  TextContent,
+  MemeGeneratorForm,
+  CustomLabel,
+  CustomInput,
+  CustomSelect,
+  CustomOption,
+  GenerateButton,
+} from './styledComponents'
+
+const fontSizesOptionsList = [
+  {
+    optionId: '8',
+    displayText: '8',
+  },
+  {
+    optionId: '12',
+    displayText: '12',
+  },
+  {
+    optionId: '16',
+    displayText: '16',
+  },
+  {
+    optionId: '20',
+    displayText: '20',
+  },
+  {
+    optionId: '24',
+    displayText: '24',
+  },
+  {
+    optionId: '28',
+    displayText: '28',
+  },
+  {
+    optionId: '32',
+    displayText: '32',
+  },
+]
+// Write your code here
+
+class MemeGenerator extends Component {
+  state = {
+    backgroundImageUrlInput: '',
+    topTextInput: '',
+    bottomTextInput: '',
+    activeFontSizeOptionId: fontSizesOptionsList[0].optionId,
+    backgroundImageUrl: '',
+    topText: '',
+    bottomText: '',
+    activeFontSizeOption: '',
+  }
+
+  onChangeBackgroundImageInput = event => {
+    this.setState({backgroundImageUrlInput: event.target.value})
+  }
+
+  onChangeTopTextInput = event => {
+    this.setState({topTextInput: event.target.value})
+  }
+
+  onChangeBottomTextInput = event => {
+    this.setState({bottomTextInput: event.target.value})
+  }
+
+  onChangeActiveOptionIdInput = event => {
+    this.setState({activeFontSizeOptionId: event.target.value})
+  }
+
+  onGenerateMeme = event => {
+    event.preventDefault()
+    const {
+      backgroundImageUrlInput,
+      topTextInput,
+      bottomTextInput,
+      activeFontSizeOptionId,
+    } = this.state
+
+    this.setState({
+      backgroundImageUrl: backgroundImageUrlInput,
+      topText: topTextInput,
+      bottomText: bottomTextInput,
+      activeFontSizeOption: activeFontSizeOptionId,
+    })
+  }
+
+  renderMemeGeneratorForm = () => {
+    const {
+      backgroundImageUrlInput,
+      topTextInput,
+      bottomTextInput,
+      activeFontSizeOptionId,
+    } = this.state
+
+    return (
+      <MemeGeneratorForm onSubmit={this.onGenerateMeme}>
+        <CustomLabel for="backgroundImageUrl">Image URL</CustomLabel>
+        <CustomInput
+          type="text"
+          id="backgroundImageUrl"
+          value={backgroundImageUrlInput}
+          onChange={this.onChangeBackgroundImageInput}
+          placeholder=" Enter the Image URL"
+        />
+
+        <CustomLabel for="topText">Top Text</CustomLabel>
+        <CustomInput
+          type="text"
+          id="topText"
+          value={topTextInput}
+          onChange={this.onChangeTopTextInput}
+          placeholder=" Enter the Top Text"
+        />
+
+        <CustomLabel for="bottomText">Bottom Text</CustomLabel>
+        <CustomInput
+          type="text"
+          id="bottomText"
+          value={bottomTextInput}
+          onChange={this.onChangeBottomTextInput}
+          placeholder=" Enter the Bottom Text"
+        />
+
+        <CustomLabel for="select">Font Size</CustomLabel>
+        <CustomSelect
+          id="select"
+          value={activeFontSizeOptionId}
+          onChange={this.onChangeActiveOptionIdInput}
+        >
+          {fontSizesOptionsList.map(eachFontSizeOption => (
+            <CustomOption
+              key={eachFontSizeOption.optionId}
+              value={eachFontSizeOption.optionId}
+            >
+              {eachFontSizeOption.displayText}
+            </CustomOption>
+          ))}
+        </CustomSelect>
+        <GenerateButton type="submit">Generate</GenerateButton>
+      </MemeGeneratorForm>
+    )
+  }
+
+  renderMeme = () => {
+    const {
+      backgroundImageUrl,
+      topText,
+      bottomText,
+      activeFontSizeOption,
+    } = this.state
+
+    return (
+      <MemeContainer data-testid="meme" backgroundImage={backgroundImageUrl}>
+        <TextContent activeFontSizeOption={activeFontSizeOption}>
+          {topText}
+        </TextContent>
+        <TextContent activeFontSizeOption={activeFontSizeOption}>
+          {bottomText}
+        </TextContent>
+      </MemeContainer>
+    )
+  }
+
+  render() {
+    return (
+      <AppContainer>
+        <MemeGeneratorContainer>
+          <Heading>Meme Generator</Heading>
+          <FormAndMemeContainer>
+            {this.renderMeme()}
+            {this.renderMemeGeneratorForm()}
+          </FormAndMemeContainer>
+        </MemeGeneratorContainer>
+      </AppContainer>
+    )
+  }
+}
+
+export default MemeGenerator
